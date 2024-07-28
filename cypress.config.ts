65de6fc14b011eeb26665fd55e466151990cc8f7
jirayu <jirayu004za@gmail.com>
2024-07-28 15:22:08 +0700

    update config

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,6 +4,12 @@ export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      config.env = config.env || {};
+      // you could extract only specific variables and rename them if necessary
+      config.env.FRONTEND_URL = process.env.FRONTEND_URL;
+      config.env.BACKEND_URL = process.env.BACKEND_URL;
+      console.log("Extended config.env with process.env");
+      return config;
     },
   },
 });
